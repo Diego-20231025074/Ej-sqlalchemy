e9e67a6b36babb8bb4a5ccf8a54a7b2ca0b03cd9
Diego Alejandro Garcia <yeyediego1200@gmail.com>
2025-06-08 17:35:57 -0500

    V 0.8

--- Estudiantes.py
+++ Estudiantes.py
@@ -6,7 +6,17 @@ class Estudiante(db.Model):
     __tablename__="estudiante"
     
     # conjunto de atributos que vana ser los campos de la tabla 
-    
+    # Llave primaria
+    id=db.Colunm(db.integer, primary_key=True)
     nombre=db.Column(db.String(50))
     email=db.Column(db.String(70))
     codigo=db.Column(db.String(15))
+    
+    #Metodo constructor para mapear datos a los cmpos definidos 
+    
+    def __init__(self, nombre, email, codigo):
+        
+        self.nombre=nombre
+        self.email=email
+        self.codigo=codigo
+     
--- Programa.py
+++ Programa.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from db import db
+from Estudiante import Estudiante
 
 
 
@@ -6,15 +8,44 @@ class Programa:
     def init(self):
         
         self .app=Flask(__name__)
-        self.app.config['SQLALCHEMY_DATABSE_URI']="sqlite:///estudiantes.sqlite3"
+        self.app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///estudiantes.sqlite3"
         
-        self.app.add_url_rule('/nuevo', view_func=self.agregar)
+        db.init_app(self.app)
+        self.app.add_url_rule('/nuevo', view_func=self.buscarTodos)
+        self.app.add_url_rule('/nuevo', view_func=self.agregar, methods=["GET", "POST"])
         
         #inicializar el servidor 
+        with self.app.app_context():
+            db.create_all()
+        
         self.app. run(debug=True)
         
         
+        def buscarTodos(self):
+            return "TO DO: "
+        
+        
         def agregar(self):
+            
+            # verficar si debe enviar los datos o procesar los datos
+            
+            if request.method=="POST":
+                
+                #Crear un objeto de la clase estudiante con los valores del formulario 
+                
+                nombre=request.form['nombre']
+                email=request.form['email']
+                codigo=request.form['codigo.']
+                miEstudiante=Estudiante(nombre, email, codigo)
+                
+                #guardar el objeto en la db
+                
+                db.session.add(miEstudiante)
+                db.session.commit()
+            
+            
+            
+            
             return render_template('nuevoEstudiantes.html')
         
         
