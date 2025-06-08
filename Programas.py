a5934684475b7a69bf13ce2fc5b68813e53f86ae
Diego Alejandro Garcia <yeyediego1200@gmail.com>
2025-06-08 16:12:53 -0500

    v 0.1

---
+++ Programas.py
@@ -0,0 +1,12 @@
+from flask import Flask 
+
+
+
+class Programa:
+    def init(self):
+        
+        self .app=Flask(name)
+        
+        self.app.add_url_rule('/nuevo', view_func=self.agregar)
+        
+        #inicializar el ser
